b2cde5d356437492b660cbecc5c8b9d5f17ad1e7
Happiness-sudo <ndolohappiness@gmail.com>
2026-04-22 08:37:18 +0300

    Refactor User and Note models
    Refactor User and Note models

--- app/models.py
+++ app/models.py
@@ -1,6 +1,5 @@
 from . import db, bcrypt
 
-# USER MODEL
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -8,16 +7,12 @@ class User(db.Model):
 
     notes = db.relationship('Note', backref='user', lazy=True)
 
-    # PASSWORD SETTER
     def set_password(self, password):
         self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
 
-    # PASSWORD CHECKER
     def check_password(self, password):
         return bcrypt.check_password_hash(self.password_hash, password)
 
-
-# NOTE MODEL (RESOURCE)
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
